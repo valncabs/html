e5b18e5f461d9cbf62dbf91e878d82c7fedb54af
valncabs <cabasmirandavalen@gamil.com>
2026-04-22 10:59:47 -0500

    advanve 3.0

--- main.js
+++ main.js
@@ -10,3 +10,21 @@ function ShowMenu(){
 
     nav[0].classList.toggle("active")
 }
+
+function MostrarContenido(){
+    var button = document.getElementById('button')
+    var caja = document.getElementsByClassName('contenedor')[0].getElementsByClassName('caja')
+
+    caja[0].classList.toggle('hidden')
+
+    caja[1].classList.toggle('hidden')
+
+    caja[2].classList.toggle('hidden')
+
+    if (caja[0].classList.contains('hidden')){
+        button.innerText = 'Mostrar';
+    }else{
+        button.innerText = 'Ocultar'
+    }
+}
+
